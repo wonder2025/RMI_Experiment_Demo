dea82c2a287adce53d10c76a4ad4016578e64290
wonder2025 <757360513@qq.com>
2017-11-29 23:20:41 +0800

    a

--- RMI_Experiment_Demo/FirstServer.java
+++ RMI_Experiment_Demo/FirstServer.java
@@ -3,6 +3,8 @@ import java.rmi.*;
 import java.rmi.server.*;
 public class FirstServer{
     public static void main(String[] args) throws Exception{
+        //直接得到注册表的引用并启动注册表
+        LocateRegistry.createRegistry(1099);
         RemoteInterface object=new FirstImpl();
         Naming.rebind("rmi://127.0.0.1/RemoteServer",object);
 		//Naming.rebind("rmi://localhost/RemoteServer",object);
